caefa90cbe490951977f2180c2020483b8da6960
Katie Vogel <katiebvogel@gmail.com>
2016-09-11 21:03:00 -0500

    re-configuring

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -3,11 +3,15 @@ var app = angular.module('baseApp', ['ngRoute']);
 
   angular.module('baseApp').config(function($routeProvider, $locationProvider){
     $routeProvider
-      .when('/tempate', {
-        templateUrl: 'views/template.html',
+      .when('/', {
+        templateUrl: 'views/index.html',
         controller: 'MainController as main'
       })
-
+      .when('/template', {
+        templateUrl: 'views/template.html',
+        controller: 'TemplateController as template'
+      })
+      
     $locationProvider.html5Mode(true);
 
   }); //end  module.config
